1b6851da23033869d20b6457cc157796f5e712d7
Shubhay Gautam <shubhaygautam@gmail.com>
2024-04-29 17:26:50 +0530

    Create sol.py

---
+++ Easy/Remove every kth node/sol.py
@@ -0,0 +1,73 @@
+#Your task is to complete this function
+#Your function should return the new head pointer
+'''
+class node:
+    def __init__(self,x):
+        self.data = x
+        self.next = None
+'''
+
+class Solution:
+    def deleteK(self, head, k):
+        #code here  
+        if k == 1:
+            return None
+            
+        temp = head
+        n = 1
+        
+        while temp and temp.next:
+            if (n+1) % k == 0:
+                temp.next = temp.next.next
+                temp = temp.next
+                n+=2
+            
+            else:
+                temp = temp.next
+                n+=1;
+                
+        return head
+        
+        
+
+
+#{ 
+ # Driver Code Starts
+class node:
+
+    def __init__(self, x):
+        self.data = x
+        self.next = None
+
+
+def createLinkedList(arr):
+    head = node(arr[0])
+    curr = head
+    for i in range(1, len(arr)):
+        new_node = node(arr[i])
+        curr.next = new_node
+        curr = curr.next
+
+    return head
+
+
+def printlist(ptr):
+    while ptr != None:
+        print(ptr.data, end=" ")
+        ptr = ptr.next
+    print()
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        k = int(input())
+
+        obj = Solution()
+        head = createLinkedList(arr)
+        new_head = obj.deleteK(head, k)
+        printlist(new_head)
+
+# } Driver Code Ends
