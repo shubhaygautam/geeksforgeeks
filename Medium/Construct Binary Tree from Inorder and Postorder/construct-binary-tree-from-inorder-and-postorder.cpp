e95a27f51ab4ec158e9fdf225a132c316609b6e7
Shubhay Gautam <shubhaygautam@gmail.com>
2024-05-04 13:58:04 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Construct Binary Tree from Inorder and Postorder/construct-binary-tree-from-inorder-and-postorder.cpp
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+/* program to construct tree using inorder and postorder traversals */
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+
+/* This funtcion is here just to test buildTreeUtil() */
+void preOrder(Node* node) {
+    if (node == NULL) return;
+
+    /* then print the data of node */
+    printf("%d ", node->data);
+
+    /* first recur on left child */
+    preOrder(node->left);
+
+    /* now recur on right child */
+    preOrder(node->right);
+}
+
+
+// } Driver Code Ends
+/* Tree node structure
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};*/
+
+class Solution
+{
+    public:
+    
+    Node *solve(int in[], int post[], int inStart, int inEnd, int pStart, int pEnd){
+        if(inStart>inEnd) return NULL;
+        if(inStart == inEnd){
+            return new Node(in[inStart]);
+        }
+        int indx = -1;
+        Node* root = new Node(post[pEnd]);
+        for(int i = inStart; i<=inEnd; i++){
+            if(in[i] == post[pEnd]){
+                indx = i;
+                break;
+            }
+        }
+        int left = indx-inStart;
+        int right = inEnd-indx;
+        
+        root->left = solve(in, post, inStart, indx-1, pStart, pStart+left-1);
+        root->right = solve(in, post, indx+1, inEnd, pEnd-right, pEnd-1);
+        
+        return root;
+    }
+
+    //Function to return a tree created from postorder and inoreder traversals.
+    Node *buildTree(int in[], int post[], int n) {
+        // Your code here
+        return solve(in, post, 0, n-1, 0, n-1);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        int in[n], post[n];
+        for (int i = 0; i < n; i++) cin >> in[i];
+        for (int i = 0; i < n; i++) cin >> post[i];
+        Solution obj;
+        Node* root = obj.buildTree(in, post, n);
+        preOrder(root);
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
