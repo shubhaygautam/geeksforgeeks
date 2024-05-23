57bd3b480c955bf7edf503f484237f3269fb003c
Shubhay Gautam <shubhaygautam@gmail.com>
2024-05-23 21:37:02 +0530

    Updated Solution - GfG to GitHub

--- Easy/K-Palindrome/kpalindrome.cpp
+++ Easy/K-Palindrome/kpalindrome.cpp
@@ -1,50 +1,52 @@
 //{ Driver Code Starts
+// Initial Template for C++
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// } Driver Code Ends
+// User function Template for C++
+class Solution{
+public:
+     int lcs(string s1, string s2) {
+    int m = s1.size();
+    vector<int> prev(m + 1, 0), cur(m + 1, 0);
+    for (int ind1 = 1; ind1 <= m; ind1++) {
+        for (int ind2 = 1; ind2 <= m; ind2++) {
+            if (s1[ind1 - 1] == s2[ind2 - 1])
+                cur[ind2] = 1 + prev[ind2 - 1];
+            else
+                cur[ind2] = max(prev[ind2], cur[ind2 - 1]); 
+        }
+        prev = cur; 
+    }
+    return prev[m]; 
+        }
+    int kPalindrome(string str, int n, int k)
+    {
+        string s = str ;
+         reverse(s.begin(),s.end());
+          int ans = str.size() -  lcs(s,str);
+          
+          return (ans<=k);
+        
+    }
+};
 
-bool is_k_palin(string s,int k);
+//{ Driver Code Starts.
 
-int main() {
-	// your code goes here
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		string s ;
-		cin>>s;
-		int k;
-		cin>>k;
-		
-		cout<<is_k_palin(s,k)<<endl;
-	}
-	return 0;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, k;
+        cin>>n>>k;
+        string str;
+        cin>>str;
+        
+        Solution ob;
+        cout<<ob.kPalindrome(str, n, k)<<endl;
+    }
+    return 0;
 }
 // } Driver Code Ends
-
-
-/*You are required to complete this function*/
-bool is_k_palin(string str,int k)
-{
-//Your code here
-    int i = 0; int j = str.size()-1;
-        while(i<j){
-            if(str[i] != str[j]){
-                if(k == 0){
-                    return 0;
-                }
-                k--;
-                if(str[i] == str[j-1]){
-                    j--;
-                    continue;
-                }
-                else{
-                    i++;
-                    continue;
-                }
-            }
-            i++;
-            j--;
-        }
-        return 1;
-}
